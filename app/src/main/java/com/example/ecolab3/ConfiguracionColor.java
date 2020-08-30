a65710c4ac0f3a56a46959d7b02b092e4229bcee
stephym <60659457+stephymelo@users.noreply.github.com>
2020-08-29 22:31:57 -0500

    yup

--- app/src/main/java/com/example/ecolab3/ConfiguracionColor.java
+++ app/src/main/java/com/example/ecolab3/ConfiguracionColor.java
@@ -1,14 +1,89 @@
 package com.example.ecolab3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ConfiguracionColor extends AppCompatActivity {
+public class ConfiguracionColor extends AppCompatActivity implements View.OnClickListener {
+
+    private ConstraintLayout mConstraintLayout;
+    private Button AzulBoton;
+    private Button BlancoBoton;
+    private Button NegroBoton;
+    private String color="white";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configuracion_color);
+
+
+        mConstraintLayout = (ConstraintLayout)findViewById(R.id.constraintLayout);
+        AzulBoton = findViewById(R.id.AzulBoton);
+        BlancoBoton = findViewById(R.id.BlancoBoton);
+        NegroBoton = findViewById(R.id.NegroBoton);
+
+
+        AzulBoton.setOnClickListener(this);
+        BlancoBoton.setOnClickListener(this);
+        NegroBoton.setOnClickListener(this);
+
+        if(color.equals("blanco")){
+            mConstraintLayout.setBackgroundColor(Color.WHITE);
+
+        }
+        if(color.equals("azul")){
+            mConstraintLayout.setBackgroundColor(Color.BLUE);
+
+        }
+        if(color.equals("negro")){
+            mConstraintLayout.setBackgroundColor(Color.BLACK);
+
+        }
     }
+
+
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.AzulBoton:
+                Intent data = new Intent();
+                data.putExtra("color","azul");
+                setResult(RESULT_OK,data);
+                finish();
+                break;
+            case R.id.BlancoBoton:
+                Intent data2 = new Intent();
+                data2.putExtra("color","blanco");
+                setResult(RESULT_OK,data2);
+                finish();
+
+                break;
+            case R.id.NegroBoton:
+                Intent data3 = new Intent();
+                data3.putExtra("color","negro");
+                setResult(RESULT_OK,data3);
+                finish();
+
+                break;
+        }
+
+
+
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/ecolab3/MainActivity.java
+++ app/src/main/java/com/example/ecolab3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ecolab3;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +14,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int CODE=0;
     private EditText nombreInput;
     private TextView promptText;
     private Button continuarBoton;
+    private ConstraintLayout constraint;
     private Button configBoton;
+    private String color;
 
 
     @Override
@@ -22,13 +28,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         nombreInput = findViewById(R.id.nombreInput);
         promptText = findViewById(R.id.promptText);
         continuarBoton = findViewById(R.id.continuarBoton);
         configBoton = findViewById(R.id.configBoton);
 
-        continuarBoton.setOnClickListener(this);
+        constraint = findViewById(R.id.constraint);
+        color="white";
 
+        continuarBoton.setOnClickListener(this);
+        configBoton.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CODE && resultCode==RESULT_OK){
+            color=data.getExtras().getString("color");
+
+            if(color.equals("blanco")){
+                constraint.setBackgroundColor(Color.WHITE);
+            }
+            if(color.equals("azul")){
+                constraint.setBackgroundColor(Color.BLUE);
+            }
+            if(color.equals("negro")){
+                constraint.setBackgroundColor(Color.BLACK);
+            }
+        }
     }
 
     public void onClick(View v) {
@@ -42,7 +71,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 i.putExtra("nombre",nombreIn);
                 startActivity(i);
                 break;
+            case R.id.configBoton:
+                Intent b=new Intent(this,ConfiguracionColor.class);
+                b.putExtra("color",color);
+                startActivityForResult(b,CODE);
+
+
+                break;
         }
+
+
+
+
     }
 
 
